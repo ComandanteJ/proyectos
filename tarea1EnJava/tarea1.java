7bc2b985ec66a4d4a90da4f3e3cc219f5b208dbf
ComandanteJ <you_cant_hide@hotmail.com>
2017-11-27 14:39:07 +0100

    Añado tarea1 (Script python) en Java

---
+++ tarea1EnJava/tarea1.java
@@ -0,0 +1,22 @@
+public class tarea1 {
+
+	public static void main(String[] args) {
+
+		String[] miarray = {"Silvia","Federico","Miguel","Julia","Evaristo"};
+
+		System.out.print(miarray[2]+"\n\n");
+
+	        // for(String st : miarray) {
+                //      Sys....
+                // }	
+		for(int i = 0; i < miarray.length; i++){
+			
+			System.out.println(miarray[i]+" es un valor contenido en el array miarray.");
+
+		}
+
+
+		
+	}
+
+}
